69f9ef3096a26e0126a673dad96f698fe732496d
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-11-22 00:09:16 +0800

    Update article.go

--- obj/article.go
+++ obj/article.go
@@ -5,7 +5,6 @@ import "time"
 type Article struct {
 	ID          uint      `json:"id" gorm:"primaryKey"`
 	Title       string    `json:"title"`
-	Icon        string    `json:"icon"`
 	Description string    `json:"description"`
 	PublishDate time.Time `json:"publishDate"`
 	UpdateDate  time.Time `json:"updateDate"`
