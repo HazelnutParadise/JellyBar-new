9bb4a16d246f8bc53b6d939534dfe60d6b36c57e
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-11-21 23:39:48 +0800

    update

--- obj/article.go
+++ obj/article.go
@@ -3,12 +3,14 @@ package obj
 type Article struct {
 	ID          uint     `json:"id"`
 	Title       string   `json:"title"`
+	Icon        string   `json:"icon"`
 	Description string   `json:"description"`
 	PublishDate string   `json:"publishDate"`
 	UpdateDate  string   `json:"updateDate"`
 	Content     string   `json:"content"`
 	Media       []string `json:"media"`
-	CategoryID  uint     `json:"categoryId"`
-	AuthorID    uint     `json:"authorId"`
-	Icon        string   `json:"icon"`
+	CategoryID  uint
+	Category    Category `json:"category" gorm:"foreignKey:CategoryID;references:ID"`
+	AuthorID    uint
+	Author      Author `json:"author" gorm:"foreignKey:AuthorID;references:ID"`
 }
--- obj/category.go
+++ obj/category.go
@@ -2,8 +2,8 @@ package obj
 
 type Category struct {
 	ID          uint      `json:"id"`
+	Icon        string    `json:"icon"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
-	Icon        string    `json:"icon"`
 	Articles    []Article `json:"articles" gorm:"constraint:OnUpdate:CASCADE;"`
 }
