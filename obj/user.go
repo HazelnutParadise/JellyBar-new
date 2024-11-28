4ed0237b09d28920a45fd0c225097124b6074011
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-11-28 19:03:29 +0800

    Update user.go

--- obj/user.go
+++ obj/user.go
@@ -5,6 +5,8 @@ import "time"
 type User struct {
 	ID             uint      `json:"id" gorm:"primaryKey"`
 	Uuid           string    `json:"uuid" gorm:"unique"`
+	AuthorID       uint      `json:"author_id"`
+	Author         Author    `json:"author" gorm:"foreignKey:AuthorID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Username       string    `json:"username"`
 	Name           string    `json:"name"`
 	Role           string    `json:"role"`
