29d46f291d163c2042174220b029d72898df0e24
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-11-29 19:44:41 +0800

    Update Navbar.svelte

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -19,6 +19,7 @@
     }
 
     .navbar {
+        height: 52px;
         background: rgba(53, 70, 73, 0.95) !important;
         backdrop-filter: blur(10px);
         border-bottom: 1px solid rgba(255, 255, 255, 0.1);
